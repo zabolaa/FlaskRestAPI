abb83f5b50a8acca19b825f373befedd6dfb27ce
aleksandrardzanek <olaa.rdzanek@gmail.com>
2026-03-16 15:41:17 +0100

    Change in file

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, Ola !'
+shops = [{"name": "Reserved", "products": [{"name": "T-shirt", "price": 35}]}]
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/shops")
+def get_shops():
+    return shops
+
+@app.route("/shops", methods=["POST"])
+def create_shop():
+    shop =request.json
+
+    shops.append(shop)
+
+    return shop, 201
+
+app.run()
